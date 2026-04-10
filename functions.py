7572d38bcdd6968fd331e1482d2e897094e860e2
Kenan <kenanabbaszade51@gmail.com>
2026-04-10 13:35:06 +0400

    add: Yeni gelir elave eleme sistemi elave edildi add: Budget reset sistemi

--- functions.py
+++ functions.py
@@ -4,10 +4,18 @@ def show_budget(data):
         print("Hələ heç bir xərc yoxdur")
     else:
         for i, xerc in enumerate(data, 1):
-            print(f"{i}. {xerc["ad"]}- {xerc["mebleg"]} AZN")
-        print("---------------------")
+            tarix_format = xerc["date"].strftime("%d.%m.%Y %H:%M")
+            print(f"{i}. {xerc["ad"]} - {xerc["mebleg"]}AZN - {tarix_format} ")
+        print("---------------------------")
 
 def calculate_balance(gelir,data):
     toplam_xerc = sum(xerc["mebleg"]for xerc in data)
     balans = gelir - toplam_xerc
     return balans, toplam_xerc
+def reset_budget(data):
+    confirmation = input("Butun siyahını temizlemeyi tesdiqleyirsinizmi No/Yes : ")
+    if confirmation == "Yes":
+        data.clear()
+        print("Bütün xərclər silindi. Proqram sıfırlandı.")
+    else:
+        print("Sıfırlama Ləğv edildi")
--- main.py
+++ main.py
@@ -1,45 +1,61 @@
-from functions import show_budget,calculate_balance
+from functions import show_budget,calculate_balance,reset_budget
+import datetime
+
 #Developed by Kenan Abbaszade
 def main():
 
-    gelir = 1000
+    global gelir
     xercler = []
+    gelirler = []
 
     print("--- Budget Tracker v1.0 ---")
     while True:
         print("\n=== Budget Planner ===")
         print("1. Xerc daxil edin")
-        print("2. Kommunal Ödənişlər")
-        print("3. Xercleri göstərin")
-        print("4. Balansı yoxla")
-        print("5. Çıxış")
+        print("2. Ayliq Gəlir daxil edin")
+        print("3. Kommunal Ödənişlər")
+        print("4. Xercleri göstərin")
+        print("5. Balansı yoxla")
+        print("6. Reset Budget")
+        print("7. Çıxış")
 
         secim = input("\nSeciminizi edin 1-4 : ")
 
         if secim == "1":
             ad = input("Xercin adi : ")
             try:
+                date = datetime.datetime.now()
                 mebleg = float(input("Meblegi : "))
-                xercler.append({"ad":ad,"mebleg":mebleg})
+                xercler.append({"ad":ad,"mebleg":mebleg,"date":date})
                 print("Ugurla elave edildi!")
             except ValueError:
                 print("Səhv: Zəhmət olmasa rəqəm daxil edin.")
         elif secim == "2":
+            ad = input("Gelir Növünün daxil edin: : ")
+            try:
+                gelir = float(input("Geliri daxil edin  : "))
+                gelirler.append({"ad":ad,"gelir":gelir})
+            except ValueError:
+                print("Error: Zəhmət olmasa Rəqəm daxil edin.")
+        elif secim == "3":
             ad = input("Kommunal xercin adi : ")
             try:
-                mebleg = float(input("Kommunal daxil edin: "))
-                xercler.append({"ad":ad,"mebleg":mebleg})
+                date = datetime.datetime.now()
+                mebleg = float(input("Kommunal xercin meblegini daxil edin: "))
+                xercler.append({"ad":ad,"mebleg":mebleg,"date":date})
                 print("Ayliq Kommunal xərci uğurla daxil edildi!")
             except ValueError:
                 print("Error: Zəhmət olmasa düzgün daxil edildiyini yoxlayin")
-        elif secim == "3":
-            show_budget(xercler)
         elif secim == "4":
+            show_budget(xercler)
+        elif secim == "5":
             balans,toplam = calculate_balance(gelir,xercler)
             print(f"\nÜmumi Gəlir: {gelir} AZN")
             print(f"Toplam Xərc: {toplam} AZN")
             print(f"Cari Balans: {balans} AZN")
-        elif secim == "5":
+        elif secim == "6":
+            reset_budget(xercler)
+        elif secim == "7":
             print("Programdan cixilir sagolun!")
             break
         else:
